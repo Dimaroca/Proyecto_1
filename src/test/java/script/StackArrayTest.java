db7face85e4af6ddb013c6918dcfed4197618aed
Dimaroca <dimateoroca@gmail.com>
2026-02-13 17:25:00 -0600

    pom.xml added

--- src/test/java/script/StackArrayTest.java
+++ src/test/java/script/StackArrayTest.java
@@ -1,8 +1,6 @@
 package script;
 
 import static org.junit.jupiter.api.Assertions.*;
-
-import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class StackArrayTest {
@@ -15,37 +13,8 @@ public class StackArrayTest {
         stack.push(20);
 
         assertEquals(20, stack.peek());
-        assertEquals(20, stack.pop());  
+        assertEquals(20, stack.pop());
         assertEquals(10, stack.pop());
         assertTrue(stack.isEmpty());
     }
-
-    @Test
-    void scriptOP1Test() {
-        Interpreter interpreter = new Interpreter();
-        boolean result = interpreter.execute(List.of(Opcode.OP_1));
-        assertTrue(result);
-    }
-
-    @Test
-    void scriptOP0Test() {
-        Interpreter interpreter = new Interpreter();
-        boolean result = interpreter.execute(List.of(Opcode.OP_0));
-        assertFalse(result);
-    }
-
-    @Test
-    void equalVerifyTest() {
-        Interpreter interpreter = new Interpreter();
-
-        boolean result = interpreter.execute(List.of(
-                Opcode.OP_2,
-                Opcode.OP_DUP,
-                Opcode.OP_EQUALVERIFY,
-                Opcode.OP_1
-        ));
-
-        assertTrue(result);
-    }
-
 }
